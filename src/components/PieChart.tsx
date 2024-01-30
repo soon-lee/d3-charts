5c00093048d588b7567434c66e67e41a2c18bf50
lixun13 <felix7858@outlook.com>
2024-01-30 18:16:03 +0800

    封装D3，预先配置DOM；切分图元分别封装

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -1,72 +1,69 @@
 import {useEffect, useRef} from "react";
-import {arc, pie, PieArcDatum, select} from "d3";
 import {Label, Legend, Marking, Title, Tooltip, Total} from "./elements.tsx";
+import {Font, TitlePlot} from "./TitlePlot.tsx";
+import {arc, pie, PieArcDatum} from "d3";
+import {PieBlock} from "./PieBlock.tsx";
 
-export interface PieItem{
-    group:string,
-    label:string,
-    value:number,
-}
-export interface PieDonut{
-    enabled:boolean,
-    rate:number
-}
-export interface PieConfig{
-    width:number,
-    height:number,
-    title:Title,
-    legend:Legend,
-    total:Total,
-    tooltip:Tooltip,
-    label:Label,
-    marking:Marking
-}
-export interface PieProps{
-    data:PieItem[],
-    donut:PieDonut,
-    config:PieConfig
+export interface PieItem {
+    group: string,
+    label: string,
+    value: number,
 }
 
-export const PieChart = ({data,donut,config}:PieProps)=>{
+export interface PieDonut {
+    enabled: boolean,
+    rate: number
+}
+
+export interface PieConfig {
+    width: number,
+    height: number,
+    title: Title,
+    legend: Legend,
+    total: Total,
+    tooltip: Tooltip,
+    label: Label,
+    marking: Marking
+}
+
+export interface PieProps {
+    data: PieItem[],
+    donut: PieDonut,
+    config: PieConfig
+}
+
+export const PieChart = ({data, donut, config}: PieProps) => {
 
     const svgRef = useRef(null);
 
     const pieLayout = pie<PieItem>().value(d => d.value);
 
-    const arcGenerator = arc<PieArcDatum<PieItem>>();
+    const arcSector = arc<PieArcDatum<PieItem>>();
+    const arcLabel = arc<PieArcDatum<PieItem>>();
+    const arcMarking = arc<PieArcDatum<PieItem>>();
 
     useEffect(() => {
-        if(!svgRef.current) return;
-        const svgTemp = select(svgRef.current);
-        svgTemp.attr('width',config.width).attr('height',config.height)
-            .append('g').attr('class','sl-title')
-            .append('g').attr('class','sl-legend')
-            .append('g').attr('class','sl-total')
-            .append('g').attr('class','sl-tooltip')
-            .append('g').attr('class','sl-label')
-            .append('g').attr('class','sl-marking')
-            .append('g').attr('class','sl-data');
-        // return ()=>svgTemp.selectAll('g').remove();
+        if (!svgRef.current) return;
+        // select(svgRef.current).
     }, []);
 
-    useEffect(() => {
-        const radius = Math.min(config.width,config.height)/2 -10;
-        arcGenerator
-            .innerRadius(donut.enabled ? donut.rate*radius : 0)
-            .outerRadius(radius);
-    }, [donut,config.width,config.height]);
-
-    useEffect(() => {
-        if(!svgRef.current) return;
-        const svgTemp = select(svgRef.current);
-        svgTemp.select('.sl-data')
-            .attr('transform',`translate(${config.width / 2}, ${config.height / 2})`)
-            .data(pieLayout(data))
-            .join('path')
-            .attr('d',arcGenerator)
-            .style('fill','green');
-        // return ()=>svgTemp.selectAll('g').remove();
-    }, [data,config.width,config.height]);
-
-    return <svg ref={svgRef} />
+    return <svg ref={svgRef}>
+        <TitlePlot
+            show={true}
+            width={100}
+            height={30}
+            color={'black'}
+            font={new Font(null)}
+            direction={'lr'}
+            rotation={0}
+            text={'config.title.text'}
+        />
+        <PieBlock
+            data={data}
+            width={config.width}
+            height={config.height}
+            donut={donut}
+            config={config}
+        />
+    </svg>
 };
--- src/components/TitlePlot.tsx
+++ src/components/TitlePlot.tsx
@@ -1,20 +1,76 @@
-import {useRef} from "react";
+export class Font {
+    family: string;
+    size: number;
+    weight: string;
+    style: string;
+    height: number;
 
-export class TitlePlotProps{
-    text: string;
-    position:string;
-    offset:string;
-    reverse:boolean;
-    constructor(props: { text: string|null;position: string|null,offset: string|null,reverse: boolean|null }) {
-        this.text = props && props.text || '标题';
-        this.position = props && props.position || 'top';
-        this.offset = props && props.offset || 'left';
-        this.reverse = props && props.reverse || false;
+    constructor(props: {
+        family?: string; size?: number; weight?: string; style?: string; height?: number;
+    } | null) {
+        this.family = props && props.family || '"Fira Code","Microsoft Yahei"';
+        this.size = props && props.size || 13;
+        this.weight = props && props.weight || 'normal';
+        this.style = props && props.style || 'normal';
+        this.height = props && props.height || 30;
     }
 }
-export const TitlePlot = ()=>{
 
-    const groupRef = useRef(null);
+export interface TitlePlotProps {
+    show: boolean;
+    text: string;
+    font: Font;
+    color: string;
+    direction: string;
+    rotation: number;
+    width: number;
+    height: number;
+}
 
-    return <g ref={groupRef} />
+export const TitlePlot = ({show, text, font, color, direction, rotation, width, height}: TitlePlotProps) => {
+    const randomChar12 = Array
+        .from({length: 12})
+        .map(() => {
+            const num = Math.random() > 0.5 ? Math.random() * 25 + 65 : Math.random() * 25 + 97;
+            String.fromCharCode(num);
+        })
+    const pathId = `path-${randomChar12.join('')}`;
+
+    const dealDirect: () => string = () => {
+        if (direction === 'rl') {
+            return `M ${width},${height / 2}L${0},${height / 2}`;
+        } else if (direction === 'tb') {
+            return `M ${width / 2},0L${width / 2},${height}`;
+        } else if (direction === 'bt') {
+            return `M ${width / 2},${height}L${width / 2},0`;
+        } else {
+            return `M 0,${height / 2}L${width},${height / 2}`;
+        }
+    }
+    const dealRotate: () => number = () => {
+        if (direction === 'rl') {
+            return 180;
+        } else if (direction === 'tb') {
+            return -90;
+        } else if (direction === 'bt') {
+            return 90;
+        } else {
+            return 0
+        }
+    }
+
+    return <g width={width} height={height} opacity={show ? 1 : 0}>
+        <path id={pathId} d={dealDirect()}/>
+        <text
+            rotate={dealRotate()}
+            letterSpacing={(direction === 'lr' || direction === 'rl') ? 0 : 10}
+            fill={color}
+            fontSize={font.size}
+            fontFamily={font.family}
+            fontStyle={font.style}
+            transform={`rotate(${rotation},${width / 2},${height / 2})`}
+        >
+            <textPath href={`#${pathId}`} textAnchor='middle'>{text}</textPath>
+        </text>
+    </g>;
 }
